e921030c1d913fb9f44a6f006cb55a8479092009
runxiu.li <runxiu.li@nio.com>
2023-11-29 15:13:24 +0800

    feat: git init

---
+++ src/test/java/com/example/esqueryframework/EsQueryFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esqueryframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsQueryFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
